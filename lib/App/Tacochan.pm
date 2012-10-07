23d4fdaafa7791c9027e503d776b2aa3de0ac27c
Takumi Akiyama <akiym@cpan.org>
2012-10-07 14:04:42 +0000

    Import of AKIYM/App-Tacochan-0.03 from CPAN.
    gitpan-cpan-distribution: App-Tacochan
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         AKIYM/App-Tacochan-0.03.tar.gz
    gitpan-cpan-author:       AKIYM
    gitpan-cpan-maturity:     released

--- lib/App/Tacochan.pm
+++ lib/App/Tacochan.pm
@@ -1,14 +1,14 @@
 package App::Tacochan;
 use strict;
 use warnings;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 1;
 __END__
 
 =head1 NAME
 
-tacochan - Skype message delivery by HTTP
+App::Tacochan - Skype message delivery by HTTP
 
 =head1 SYNOPSIS
 
